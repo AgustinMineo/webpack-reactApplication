b50e9ca8a47393d4c628fc514334aac7b5097279
AgustinMineo <mineoagustin@gmail.com>
2023-05-01 18:19:31 -0300

    update with webpack for dev and prod

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,23 +1,20 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
 
 module.exports ={
     entry:'./src/index.js', //Configuramos el entry del project, normalmente esta ne src/index.js
     output:{
         path: path.resolve(__dirname,'dist'), //Declaramos la carpeta raiz, en este caso dist
         filename: 'bundle.js',
-        publicPath: '/',
     },
     resolve:{
         extensions : ['.js','.jsx'],//Declaramos las extensiones que vamos a utilizar
         alias:{
             '@components': path.resolve(__dirname,'src/components/'),
             '@styles': path.resolve(__dirname,'src/styles/'),
-        }
+        },
     },
     mode : 'development',
     module:{
@@ -52,10 +49,10 @@ module.exports ={
        }),
        new MiniCssExtractPlugin({
         filename: '[name].css'
-       }) 
+       }),
     ],
     devServer : { //Configuración del servidor de desarollo.
-        static:{directory: path.join(__dirname, 'dist'),},
+        static:{directory: path.join(__dirname, 'dist')},
         compress: true,
         port : 3006
     }
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,25 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports ={
     entry:'./src/index.js', //Configuramos el entry del project, normalmente esta ne src/index.js
     output:{
         path: path.resolve(__dirname,'dist'), //Declaramos la carpeta raiz, en este caso dist
         filename: 'bundle.js',
+        publicPath: "./",
     },
     resolve:{
-        extensions : ['.js','.jsx']//Declaramos las extensiones que vamos a utilizar
+        extensions : ['.js','.jsx'],//Declaramos las extensiones que vamos a utilizar
+        alias: {
+            '@components': path.resolve(__dirname,'src/components/'),
+            '@styles': path.resolve(__dirname,'src/styles/'),
+        }
     },
+    mode : 'production',
     module:{
         rules : [ //Reglas y extenciones que vamos a utilizar
             {
@@ -39,10 +48,18 @@ module.exports ={
     plugins: [
        new HtmlWebPackPlugin({
         template:'./public/index.html',
-        filename:'./index.html',
+        filename:'./index.html'
        }),
        new MiniCssExtractPlugin({
         filename: '[name].css'
-       }) 
-    ]
+       }),
+       new CleanWebpackPlugin(),
+    ],
+    optimization:{
+        minimize:true,
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin(),
+        ]
+    }
 }
