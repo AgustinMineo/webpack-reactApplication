2a1c34baa261cdb50ca70f7bd25478a08420111a
AgustinMineo <mineoagustin@gmail.com>
2023-05-01 17:43:49 -0300

    add alias

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,16 +3,21 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const {CleanWebpackPlguin} = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports ={
     entry:'./src/index.js', //Configuramos el entry del project, normalmente esta ne src/index.js
     output:{
         path: path.resolve(__dirname,'dist'), //Declaramos la carpeta raiz, en este caso dist
         filename: 'bundle.js',
+        publicPath: '/',
     },
     resolve:{
         extensions : ['.js','.jsx'],//Declaramos las extensiones que vamos a utilizar
+        alias:{
+            '@components': path.resolve(__dirname,'src/components/'),
+            '@styles': path.resolve(__dirname,'src/styles/'),
+        }
     },
     mode : 'development',
     module:{
